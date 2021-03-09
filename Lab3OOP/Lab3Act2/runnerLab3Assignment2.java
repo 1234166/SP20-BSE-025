b28e981806cb10ae5e9d79e5d4df3a96ccd2c7c2
1234166 <80268056+1234166@users.noreply.github.com>
2021-03-09 13:34:12 +0500

    Add files via upload

---
+++ Lab3OOP/Lab3Act2/runnerLab3Assignment2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1a2runner;
+
+/**
+ *
+ * @author cui
+ */
+public class runnerLab3Assignment2 {
+    public static void main(String []args){
+        time t = new time();
+        t.show();
+    }
+}
